114b3ff0322a97532b7ce52e438ac451351a809e
Kaio Gabriel <kaiogabrielrozini@gmail.com>
2023-11-16 20:32:32 +0000

    adicionado o link no console.log do site

--- server.js
+++ server.js
@@ -55,5 +55,5 @@ sockets.on("connection", (socket) => {
 
 
 server.listen(3000, () => {
-  console.log(`> Server listening on port: 3000`);
+  console.log(`> Server listening on port: http://localhost:3000`);
 });
